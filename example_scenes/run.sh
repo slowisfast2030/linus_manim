ae811b4f841678f2f7ae283fbe9a78b15d778b4e
slowisfast <slowisfast2030@163.com>
2022-08-31 22:01:23 +0800

    all is well

---
+++ example_scenes/run.sh
@@ -0,0 +1,7 @@
+# 显示场景 StationaryDistPreview
+echo scene: $1
+# anaconda python环境
+# 新建了虚拟环境manim 
+# conda activate manim
+# python -m manim render markov_chain.py $1 -c manim.cfg
+python -m manim opengl.py $1 -c manim.cfg
